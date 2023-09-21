dc37d5d1d2ad8657a4ebbb2677fb5aae6e882da7
Rasrizal96 <rasrizalrosdi96@gmail.com>
2023-09-21 09:55:18 +0800

    update cuba config

---
+++ modules/web/src/com/company/clinic/web/screens/newscreen/NewScreen.java
@@ -0,0 +1,10 @@
+package com.company.clinic.web.screens.newscreen;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("clinic_newscreen")
+@UiDescriptor("new-screen.xml")
+public class NewScreen extends Screen {
+}
